4345a56ea7ce42b360489276d269abd5d28f96e5
invernoe <mahmoudk221999@gmail.com>
2024-02-07 23:58:21 +0200

    add bootstrap and font awesome

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//font awesome
+import "@fortawesome/fontawesome-free/css/all.min.css"
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
